6ca0a4fa5a0cdb3c58650058197f590e81260275
lyubo <lubomir.angelov.an@gmail.com>
2016-05-25 12:11:42 +0300

    added create user file

---
+++ createUser.js
@@ -0,0 +1,9 @@
+POST https://www.googleapis.com/admin/directory/v1/users
+{
+ "name": {
+  "familyName": "TheThird",
+  "givenName": "Admin"
+ },
+ "password": "Chrome1!",
+ "primaryEmail": "admin3@techteam51.info"
+}
